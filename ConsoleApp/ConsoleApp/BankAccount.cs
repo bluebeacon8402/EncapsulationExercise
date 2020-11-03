45587021583954f84e0e437fe5e25d54f557b463
bluebeacon8402 <crsorge@icloud.com>
2020-11-03 12:35:27 -0500

    new exercise

---
+++ ConsoleApp/ConsoleApp/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account");
+
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much money would you like to deposit?");
+
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is {userBalance,0:c}");
+        }
+    }
+}
